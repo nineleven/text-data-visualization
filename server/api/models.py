78c859864d3bffcf68ce3168124cdce931cc3d3f
nineleven <tim-sania2008@mail.ru>
2021-05-25 21:27:32 +0300

    add postgres support

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class SampleText(models.Model):
+
+    name = models.CharField(max_length=40)
+
+    text = models.CharField(max_length=400)
