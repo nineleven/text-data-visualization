322b113fa7a248e4e9e7154dec2674f8cfaa5961
nineleven <tim-sania2008@mail.ru>
2021-05-25 12:45:13 +0300

    add sample text selection

--- rshiny/app.R
+++ rshiny/app.R
@@ -1,21 +1,25 @@
 library(shiny)
 library(plotly)
+library(httr)
+
+
+TEXT_AREA_START_HEIGHT <- 200
+GET_AVAILABLE_TEXTS_URL <- 'http://127.0.0.1:8000/api/get_available_texts/'
+GET_TEXT_BY_NAME_URL <- 'http://127.0.0.1:8000/api/get_text/'
+
 
 ui <- pageWithSidebar(
   
   headerPanel('TextViz'),
   
   sidebarPanel(
-    selectInput('sample_texts', 'Sample texts:',
-                c('Text1'='text one', 
-                  'Text2'='text two')
-                ),
+    uiOutput('sample_text'),
     
     actionButton('load_sample_text', 'load sample'),
     
-    textAreaInput('input_text', 'Your text:', height=200),
+    textAreaInput('input_text', 'Your text:', height=TEXT_AREA_START_HEIGHT),
     
-    actionButton('submit', 'Vizualize text'),
+    actionButton('submit', 'Vizualize text')
   ),
   
   mainPanel(
@@ -23,15 +27,37 @@ ui <- pageWithSidebar(
   )
 )
 
-xs <- c(1, 2, 3, 4)
-ys <- c(2, 3, 2, 5)
-words <- c('w1', 'w2', 'w3', 'w4')
-encoded_words <- data.frame(X=xs, Y=ys, W=words)
+get_available_texts <- function() {
+  response <- GET(GET_AVAILABLE_TEXTS_URL)
+  json_data = content(response, as='parsed')
+  return(json_data$context)
+}
+
+get_text_by_name <- function(text_name) {
+  url <- paste(GET_TEXT_BY_NAME_URL, text_name, sep='')
+  response <- GET(url)
+  json_data = content(response, as='parsed')
+  return(json_data$context)
+}
 
 server <- function(input, output) {
+  available_texts <- reactive({
+    get_available_texts()
+  })
+  
+  output$sample_text <- renderUI({
+    selectInput('sample_text', 'Sample texts:', available_texts())
+  })
+  
+  observeEvent(input$load_sample_text, {
+    updateTextAreaInput(inputId='input_text', 
+                        value=get_text_by_name(input$sample_text))
+  })
+  
   output$wordsPlot <- renderPlotly({
-    req(encoded_words)
-    plot_ly(encoded_words, x=~X, y=~Y, text=~W, type='scatter', mode='markers', hoverinfo='test')
+    encoded_words = data.frame(X=c(1, 2, 3), Y=c(1, 2, 3), W=c('1', '2', '3'))
+    plot_ly(encoded_words, x=~X, y=~Y, text=~W, 
+            type='scatter', mode='markers', hoverinfo='test')
   })
 }
 
