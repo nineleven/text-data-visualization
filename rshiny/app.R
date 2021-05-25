1620dee58421b6f9a6c5c68858c3c7911c1e5ddd
nineleven <tim-sania2008@mail.ru>
2021-05-25 14:56:40 +0300

    add encoding api requests and pca

--- rshiny/app.R
+++ rshiny/app.R
@@ -4,8 +4,10 @@ library(httr)
 
 
 TEXT_AREA_START_HEIGHT <- 200
+
 GET_AVAILABLE_TEXTS_URL <- 'http://127.0.0.1:8000/api/get_available_texts/'
 GET_TEXT_BY_NAME_URL <- 'http://127.0.0.1:8000/api/get_text/'
+ENCODE_TEXT_URL <- 'http://127.0.0.1:8000/api/encode_text/'
 
 
 ui <- pageWithSidebar(
@@ -29,15 +31,22 @@ ui <- pageWithSidebar(
 
 get_available_texts <- function() {
   response <- GET(GET_AVAILABLE_TEXTS_URL)
-  json_data = content(response, as='parsed')
+  json_data <- content(response, as='parsed')
   return(json_data$context)
 }
 
 get_text_by_name <- function(text_name) {
   url <- paste(GET_TEXT_BY_NAME_URL, text_name, sep='')
   response <- GET(url)
-  json_data = content(response, as='parsed')
+  json_data <- content(response, as='parsed')
   return(json_data$context)
+}
+
+encode_text <- function(text) {
+  response <- POST(ENCODE_TEXT_URL,
+                   body = text)
+  json_data <- content(response, as='parsed')
+  return(json_data)
 }
 
 server <- function(input, output) {
@@ -54,11 +63,28 @@ server <- function(input, output) {
                         value=get_text_by_name(input$sample_text))
   })
   
-  output$wordsPlot <- renderPlotly({
-    encoded_words = data.frame(X=c(1, 2, 3), Y=c(1, 2, 3), W=c('1', '2', '3'))
-    plot_ly(encoded_words, x=~X, y=~Y, text=~W, 
-            type='scatter', mode='markers', hoverinfo='test')
+  observeEvent(input$submit, {
+    wordscodes <- encode_text(input$input_text)
+    
+    num_codes = length(wordscodes$codes)
+    codes_matrix = matrix(unlist(wordscodes$codes), num_codes)
+    
+    pca_res = prcomp(x=codes_matrix, rank=2)
+    
+    reduced_codes = pca_res$x
+    
+    colnames(reduced_codes) <- c('X', 'Y')
+    
+    encoded_words = data.frame(reduced_codes)
+    encoded_words$W = wordscodes$words
+    
+    output$wordsPlot <- renderPlotly({
+      
+      plot_ly(encoded_words, x=~X, y=~Y, text=~W, 
+              type='scatter', mode='markers', hoverinfo='test')
+    })
   })
+  
 }
 
 shinyApp(ui, server)
